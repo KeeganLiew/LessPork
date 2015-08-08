37a875794cb5aceaa3aba94910a30c0caacdbf7e
ZilvaX <Kevin.x.g@gmail.com>
2015-08-09 09:19:18 +1200

    Should work now
    more changes to feeback.js trying to create an object

--- LikeABus/LikeABus/Scripts/LikeABus/feedback.js
+++ LikeABus/LikeABus/Scripts/LikeABus/feedback.js
@@ -1,22 +1,32 @@
 ﻿$(document).ready(submitForm());
-/*
-function person(rating, comment) {
+
+function person(rating, comment, latitude, longitude) {
     this.rating = rating;
     this.comment = comment;
+    this.latitude = latitude;
+    this.longitude = longitude;
+    //this.time = date();
 }
-*/
+
 function submitForm() {
     console.log("opened script file");
 }
 function myFunction() {
-    console.log("Hello myFunction()s")
+    //console.log("Hello myFunction()s");        
+    var submit = new person("","","","");
     if (document.getElementById('up').checked) {
-        console.log("UP");
+        submit.rating = "up";
+        //console.log("UP");
     }
     if (document.getElementById('down').checked) {
-        console.log("DOWN");
+        submit.rating = "down"
+        //console.log("DOWN");
     }
-    console.log(document.getElementById('textArea').value);
+    console.log(submit.rating);
+    submit.comment = document.getElementById('textArea').value
+    console.log(submit.comment);
+    console.log(submit.time);
+
 }
 
 
