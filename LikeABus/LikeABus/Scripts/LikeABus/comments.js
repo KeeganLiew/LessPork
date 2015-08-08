1817174f55be4df2ac2dc1cf78b6b73ad8548ed9
nary-dev <nary_silver@hotmail.com>
2015-08-09 11:51:52 +1200

    added show bus function

---
+++ LikeABus/LikeABus/Scripts/LikeABus/comments.js
@@ -0,0 +1,10 @@
+﻿function showBus(number, route) {
+    var display = $("#display");
+    var content = "<div class = 'bus'>" +
+        "<p>" + number + "</p>" +
+        "<p>" + route + "</p>" +
+        "<button>submit</button>" +
+        "</div>";
+    display.append(content);
+};
+
